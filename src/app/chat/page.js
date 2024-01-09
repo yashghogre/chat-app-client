f563597cbd5e180568ca6150e1d6f20f58154403
Yash <yashghogre100@gmail.com>
2024-01-09 21:16:09 +0530

    Added names

--- src/app/chat/page.js
+++ src/app/chat/page.js
@@ -14,8 +14,14 @@ export default function Home() {
 
   const [msg, setMsg] = useState('')
   const [inMsg, setInMsg] = useState([])
+  const [name, setName] = useState('Anon')
   // const [position, setPosition] = useState('')
   const lastMsg = useRef(null)
+
+  function test() {
+    setName(window.prompt('Enter a name to chat: '))
+    localStorage.setItem('name', name)
+  }
 
   useEffect(() => {
     const handleConnect = () => {
@@ -24,6 +30,8 @@ export default function Home() {
     };
 
     socket.on('connect', handleConnect);
+
+    // test();
 
     return () => {
       socket.off('connect', handleConnect);
@@ -36,7 +44,10 @@ export default function Home() {
       setInMsg((prev) => [...prev, {
         msg: msg.msg,
         pos: socket.id === msg.id ? 'right' : 'left',
-        fD: socket.id === msg.id ? 'row-reverse' : 'row'
+        fD: socket.id === msg.id ? 'row-reverse' : 'row',
+        name: msg.name,
+        namePos: socket.id === msg.id ? 'flex-end' : 'flex-start',
+        // bgCol: socket.id === msg.id ? 'alice-blue' : 'rgba(0, 255, 0, 0.151)'
       }])
       // if (msg.id === socket.id) {
       //   setPosition('right')
@@ -60,7 +71,10 @@ export default function Home() {
   const send = (e) => {
     // e.preventDefault();
     // console.log(msg)
-    socket.emit('msg', { msg: msg, id: socket.id })
+    // localStorage.setItem('name', 'Yash')
+    // setName(localStorage.getItem('name'))
+    console.log(name)
+    socket.emit('msg', { msg: msg, id: socket.id, name: name })
     setMsg('')
   }
 
@@ -71,9 +85,20 @@ export default function Home() {
         {
           inMsg.map((value, key) => {
             return (
-              <div key={key} className={styles.msgDiv} style={{ textAlign: `${value.pos}`, display: 'flex', flexDirection: `${value.fD}`, alignItems: 'center', gap: '10px' }}>
-                <FaRegUserCircle size={25} />
-                <p key={key} style={{ color: 'black', display: 'flex', gap: '1vw', alignItems: 'center', display: 'inline' }}>{value.msg}</p>
+              <div key={key} className={styles.msgDiv} >
+
+                <div className={styles.sMDiv} style={{ display: 'flex', flexDirection: 'column' }}>
+
+                  <div className={styles.nameDiv} style={{ display: 'flex', justifyContent: `${value.namePos}` }}>
+                    <h4>{value.name}</h4>
+                  </div>
+
+                  <div className={styles.sSMDiv} style={{ textAlign: `${value.pos}`, display: 'flex', flexDirection: `${value.fD}`, alignItems: 'center', gap: '10px' }}>
+                    <FaRegUserCircle size={25} />
+                    <p key={key} style={{ color: 'black', display: 'flex', gap: '1vw', alignItems: 'center', display: 'inline' }}>{value.msg}</p>
+                  </div>
+
+                </div>
                 <div ref={lastMsg} />
               </div>)
           })
@@ -83,8 +108,8 @@ export default function Home() {
         <div className={styles.inputDiv}>
           <input type='textarea' value={msg} onChange={(e) => setMsg(e.target.value)} className={styles.input} placeholder='Type your Message' />
           {msg.length === 0 ?
-            <button className={styles.btn}><AiOutlineSend color='lightGray' size={25} /></button> :
-            <button onClick={send} className={styles.btn}><AiOutlineSend size={25} /></button>
+            <button className={styles.btn}><AiOutlineSend color='lightGray' size={35} /></button> :
+            <button onClick={send} className={styles.btn}><AiOutlineSend size={35} /></button>
           }
         </div>
       </div>
